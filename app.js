89cd45753baeef55c42b6334b3c260191cc201eb
Deepanshu Gupta <deepanshuguptacode@outlook.com>
2024-06-25 23:35:53 +0530

    almost complete but some bugs left

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-import express from "express"
-import mongoose from "mongoose"
-import {route} from "./routes/route.js"
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors'; // Import the cors package
+import { route } from './routes/route.js';
 
-const app=express()
+const app = express();
 
-// const shortUrl=require("./models/shortUrl")
-// app.set("view engine","ejs")
-// app.set("views",path.join(__dirname,"views"))
-mongoose.connect("mongodb://localhost:27017/shorturl").then(()=>{
-    console.log("connected to mongo")
-}).catch((err)=>{
-    console.log(err)
-})
+// Use cors middleware
+app.use(cors());
 
-// app.use(express.urlencoded({extended:false}))
-app.use(express.json())
-route(app)
+mongoose.connect('mongodb://localhost:27017/shorturl')
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('MongoDB connection error:', err);
+    });
 
-app.listen(5000,()=>{
-    console.log("connected to server at PORT : 5000")
-})
+app.use(express.json());
+route(app);
+
+app.listen(5000, () => {
+    console.log('Server running at http://localhost:5000');
+});
